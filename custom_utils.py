b94c145d2c288e124be46da2634ccb26f1593fa4
Mikael Yeghiazaryan <mikaegiazaryan2000@gmail.com>
2022-04-19 12:28:41 +0400

    New structure introduced

---
+++ custom_utils.py
@@ -0,0 +1,63 @@
+import albumentations as A
+import cv2
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from albumentations.pytorch import ToTensorV2
+from config import DEVICE, CLASSES
+plt.style.use('ggplot')
+# this class keeps track of the training and validation loss values...
+# ... and helps to get the average for each epoch as well
+class Averager:
+    def __init__(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+        
+    def send(self, value):
+        self.current_total += value
+        self.iterations += 1
+    
+    @property
+    def value(self):
+        if self.iterations == 0:
+            return 0
+        else:
+            return 1.0 * self.current_total / self.iterations
+    
+    def reset(self):
+        self.current_total = 0.0
+        self.iterations = 0.0
+class SaveBestModel:
+    """
+    Class to save the best model while training. If the current epoch's 
+    validation loss is less than the previous least less, then save the
+    model state.
+    """
+    def __init__(
+        self, best_valid_loss=float('inf')
+    ):
+        self.best_valid_loss = best_valid_loss
+        
+    def __call__(
+        self, current_valid_loss, 
+        epoch, model, optimizer
+    ):
+        if current_valid_loss < self.best_valid_loss:
+            self.best_valid_loss = current_valid_loss
+            print(f"\nBest validation loss: {self.best_valid_loss}")
+            print(f"\nSaving best model for epoch: {epoch+1}\n")
+            torch.save({
+                'epoch': epoch+1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                }, 'outputs/best_model.pth')
+
+def save_model(epoch, model, optimizer):
+    """
+    Function to save the trained model till current epoch, or whenver called
+    """
+    torch.save({
+                'epoch': epoch+1,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                }, 'outputs/last_model.pth')
